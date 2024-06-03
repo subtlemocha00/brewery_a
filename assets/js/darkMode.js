860cef309d273f092d8770abace15d048eb74db3
subtlemocha00 <kturley@gmail.com>
2024-06-02 21:04:07 -0400

    initial commit

---
+++ assets/js/darkMode.js
@@ -0,0 +1,19 @@
+// window.addEventListener("DOMContentLoaded", (e) => {
+// 	const toggleSwitch = document.getElementById("toggleSwitch");
+// 	toggleSwitch.addEventListener("click", () => toggleDarkMode());
+// });
+
+// const toggleDarkMode = () => {
+// 	const titleText = document.querySelector(".titleText");
+// 	const sectionTitle = document.querySelectorAll(".sectionTitle");
+// 	const copyText = document.querySelectorAll(".copyText");
+// 	const footer = document.querySelector("#footer");
+
+// 	document.body.classList.toggle("darkMode");
+// 	if (titleText) titleText.classList.toggle("darkMode");
+// 	if (footer) footer.classList.toggle("darkMode");
+// 	if (sectionTitle)
+// 		sectionTitle.forEach((element) => element.classList.toggle("darkMode"));
+// 	if (copyText)
+// 		copyText.forEach((element) => element.classList.toggle("darkMode"));
+// };
