9911f880e6a6cd643048e0b816dfb9771cd7df4e
subtlemocha00 <kturley@gmail.com>
2024-06-22 01:26:20 -0400

    built upon products page. added modal window

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const fadeElements = document.querySelectorAll('.fade-in');
     fadeElements.forEach(el => observer.observe(el));
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('click', function (event) {
+        const navbar = document.querySelector('.navbar'); // Select the navbar element
+        const isClickInside = navbar.contains(event.target); // Check if the click is inside the navbar
+        const navbarToggler = document.querySelector('.navbar-toggler'); // Select the navbar toggler button
+        const navbarCollapse = document.querySelector('.navbar-collapse'); // Select the collapsible part of the navbar
+
+        if (!isClickInside && navbarCollapse.classList.contains('show')) { // If the click is outside and navbar is expanded
+            navbarToggler.click(); // Trigger a click on the toggler to collapse the navbar
+        }
+    });
+});
