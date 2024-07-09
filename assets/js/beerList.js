8ac000c2fc35bc2d40698aff91d7fc956428cb37
subtlemocha00 <kturley@gmail.com>
2024-07-08 23:01:43 -0400

    updated product page to have items added via js.

---
+++ assets/js/beerList.js
@@ -0,0 +1,103 @@
+const beerList = [
+    {
+        name: 'Properly Pixelated',
+        style: 'Juicy IPA',
+        abv: '5.9%',
+        description: 'A vibrant Juicy IPA bursting with tropical fruit flavors and a smooth, hoppy finish. Perfect for those who crave a well-balanced, refreshing brew.',
+        price: { single: '$3.50', six: '$14.00', case: '$48.00' },
+        image: './assets/images/pixel_ale.webp',
+        id: '101'
+    },
+    {
+        name: 'Snowbird Lager',
+        style: 'Canadian Lager',
+        abv: '5.0%',
+        description: 'Crisp and clean, this Canadian Lager offers a refreshing escape with its light body and smooth finish, ideal for any occasion.',
+        price: { single: '$3.00', six: '$12.50', case: '$45.00' },
+        image: './assets/images/beer_generic.webp',
+        id: '102'
+    },
+    {
+        name: 'Knuckle Puck(er)',
+        style: 'Maple Kettle Sour',
+        abv: '4.0%',
+        description: 'A unique blend of tart and sweet, this Maple Kettle Sour delivers a puckering punch with a hint of maple, making every sip an adventure.',
+        price: { single: '$3.00', six: '$12.50', case: '$45.00' },
+        image: './assets/images/stencil_lager.webp',
+        id: '103'
+    },
+    {
+        name: 'Blazing Trails',
+        style: 'American Pale Ale',
+        abv: '4.5%',
+        description: 'An American Pale Ale with a bold hop character, complemented by citrus and pine notes. Perfect for those who love a classic, flavorful brew.',
+        price: { single: '$3.00', six: '$12.50', case: '$45.00' },
+        image: './assets/images/can_a.png',
+        id: '104'
+    },
+    {
+        name: 'Midnight Rambler',
+        style: 'Imperial Stout',
+        abv: '4.5%',
+        description: 'Rich and robust, this Imperial Stout boasts deep chocolate and coffee flavors with a velvety smooth finish. A true indulgence for stout enthusiasts.',
+        price: { single: '$3.00', six: '$12.50', case: '$45.00' },
+        image: './assets/images/moon_can_a.png',
+        id: '105'
+    },
+    {
+        name: 'Devil\'s Delight',
+        style: 'Belgian Dubbel',
+        abv: '7.5%',
+        description: 'A rich and malty Belgian Dubbel with notes of dark fruit, caramel, and a touch of spice. This devilishly delightful brew is smooth and warming, perfect for savoring slowly.',
+        price: { single: '$3.00', six: '$12.50', case: '$45.00' },
+        image: './assets/images/devil_can_a.png',
+        id: '106'
+    },
+    {
+        name: 'Golden Grove',
+        style: 'Blonde Ale',
+        abv: '4.5%',
+        description: 'Light and refreshing, this Blonde Ale features a subtle malt sweetness and a crisp, clean finish. An easy-drinking beer for any time of the day.',
+        price: { single: '$3.00', six: '$12.50', case: '$45.00' },
+        image: './assets/images/can_b.png',
+        id: '107'
+    },
+    {
+        name: 'Bad Seas',
+        style: 'New England IPA',
+        abv: '6.2%',
+        description: 'A New England IPA with a hazy appearance and juicy tropical fruit flavors. Soft on the palate with a smooth, creamy finish.',
+        price: { single: '$3.00', six: '$12.50', case: '$45.00' },
+        image: './assets/images/badseas.png',
+        id: '108'
+    },
+    {
+        name: 'Zenjoy',
+        style: 'Japanese Rice Lager',
+        abv: '4.5%',
+        description: 'Crisp and refreshing, this Japanese Rice Lager offers a delicate balance of subtle malt sweetness and a clean, smooth finish. Perfect for moments of tranquility and relaxation.',
+        price: { single: '$3.00', six: '$12.50', case: '$45.00' },
+        image: './assets/images/zenjoy.png',
+        id: '109'
+    },
+    {
+        name: 'Frogue',
+        style: 'Psychedelic Amber',
+        abv: '5.4%',
+        description: 'This Amber Ale offers a perfect balance of caramel malt sweetness and a hint of bitterness. A well-rounded beer with a rich, amber hue.',
+        price: { single: '$3.00', six: '$12.50', case: '$45.00' },
+        image: './assets/images/frogue_a.png',
+        id: '110'
+    },
+    {
+        name: 'Citrus Serenade',
+        style: 'Wheat Beer',
+        abv: '5.3%',
+        description: 'A refreshing Wheat Beer with bright citrus notes and a hint of spice. Perfect for warm days and relaxing moments.',
+        price: { single: '$3.00', six: '$12.50', case: '$45.00' },
+        image: './assets/images/three_cans_b.png',
+        id: '111'
+    },
+];
+
+export default beerList;
--- assets/js/products.js
+++ assets/js/products.js
@@ -1,16 +1,90 @@
+import beerList from './beerList.js';
+
+const incrementQty = () => {
+    // const beerId = document.getAttribute('data-name')
+    console.log("beerProducts")
+    const inputQty = document.querySelectorAll('input')
+    console.log(inputQty)
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const addButton = document.querySelectorAll('addButton');
     document.querySelectorAll('button.border-a').forEach(button => {
         button.addEventListener('click', () => { incrementQty() })
     })
     // console.log(infoButtons)
+    const beerProducts = document.querySelectorAll('.beer-item')
+    beerProducts.forEach(beer => beer.addEventListener('click', () => console.log(beer.dataset.id)))
+
+
+    const productUl = document.getElementById('productUl');
+    const productsDiv = document.getElementById('products');
+    const placeholderDiv = document.createElement('div');
+    placeholderDiv.classList.add("col-12", "col-md-4", "my-3")
+    console.log(beerList)
+    beerList.map(beer => {
+        const beerCard = document.createElement('li');
+        beerCard.classList.add("col-12", "col-md-4", "my-3")
+        beerCard.innerHTML = `
+                <div class="card p-1 bg-b rounded-0 border-c h-100">
+                    <img src="${beer.image}" alt="Properly Pixelated" class="img-fit-h p-1 fade-in">
+                    <div class="card-body text-c d-flex flex-column h-100">
+                        <h5 class="fw-bold mb-3">${beer.name}</h5>
+                        <button id="" class="btn text-a border-a rounded-0 mt-auto" data-bs-toggle="modal"
+                            data-bs-target="#${beer.id}">More Info</button>
+                    </div>
+                </div>`
+
+        const modal = document.createElement('div');
+        modal.innerHTML = `
+        <div class="modal fade" id=${beer.id} tabindex="-1" aria-labelledby="beer-card" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-width border-c container row mx-auto bg-b"
+                role="document">
+                <div class="modal-content bg-b border-0 p-3 d-flex flex-md-row">
+                    <div class="col-12 col-md-5 mb-2 border-c">
+                        <img class="img-fit" src="${beer.image}" alt="product image">
+                    </div>
+                    <div class="col-12 col-md-7 mt-2 mt-md-0 d-flex flex-column ps-md-5">
+                        <div class="beer-item" data-id="b1">
+                            <h2 class="">${beer.name}</h2>
+                            <h4 class="text-e">${beer.style}</h4>
+                            <h6 class="text-e">${beer.abv} ABV</h6>
+                            <p class="mt-2">${beer.description}</p>
+                            <div class="row my-4 justify-content-between">
+                                <div class="size col btn bg-b text-c border-c text-nowrap">Single</div>
+                                <div class="size col btn bg-b text-c border-c text-nowrap">6-Pack</div>
+                                <div class="size col btn bg-b text-c border-c text-nowrap">24-Pack</div>
+                            </div>
+                            <div class="row justify-content-center">
+                                <button class="btn bg-c rounded-circle w-auto">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <label class="visually-hidden form-check-label" for="b${beer.id}">Quantity</label>
+                                <input id="b${beer.id}" data-id=${beer.id} type="number" value="1" min="1" max="100" class="w-auto text-center mx-3">
+                                <button class="btn bg-c rounded-circle w-auto" onclick="incrementQty()">
+                                    <i class="fas fa-plus"></i>
+                                </button>
+                            </div>
+                            <div class="row justify-content-center">
+                                <a href="#" class="btn bg-c rounded-pill p-sm-3 px-sm-5 mx-auto mt-3 w-auto">Add to Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+        productUl.append(beerCard)
+        productsDiv.append(modal)
+    })
+    if (beerList.length % 3 === 2) {
+        productUl.append(placeholderDiv)
+    }
+    if (beerList.length % 3 === 1) {
+        productUl.append(placeholderDiv)
+        productUl.append(placeholderDiv)
+    }
+
+
 })
 
-const incrementQty = () => {
-    const beerProducts = document.querySelectorAll('.beer-item')
-    // const beerId = document.getAttribute('data-name')
-    console.log(beerProducts)
-    beerProducts.forEach(beer => beer.addEventListener('click', () => console.log(beer.dataset.id)))
-    // const inputQty = document.querySelectorAll('input')
-    // console.log(inputQty)
-}
